cdcc8c54cc5a5ecad120b98ced0c7442042a94a5
Steven Heung <flyrobot27@gmail.com>
2020-10-21 17:06:11 -0600

    better failcheck

--- Code/SystemFunctions.py
+++ Code/SystemFunctions.py
@@ -1,11 +1,13 @@
 import sqlite3
 import time
+from datetime import datetime
 import curses
 import curses.textpad
 
-def editor():
+def editor(pretitle="", prebody=""):
     '''
     A simple editor for posting questions or answers. Returns the title and body of the post
+    It can also edit post given the title and body of the post wanted to be modified
     '''
 
     # initialize command line text editor with curses
@@ -14,32 +16,46 @@ def editor():
     curses.cbreak()      # no Enter key required
     stdscr.keypad(True)  # enable special keys
 
-    begin_x = 0
-    begin_y = 0
-    rows, cols = stdscr.getmaxyx()
-    win = curses.newwin(rows, cols, begin_y, begin_x)
+    begin_x = 0         # starting x coordinate
+    begin_y = 0         # starting y coordinate
+    rows, cols = stdscr.getmaxyx()   # get display dimention of the console
+
+    win = curses.newwin(rows, cols, begin_y, begin_x)   # create window
     win.addstr(begin_y, begin_x, "Type your post here. Press Ctrl + G to switch and exit.")
     win.addstr(begin_y + 1, begin_x, "Title:")
     win.addstr(begin_y + 3, begin_x, "Body:")
     win.refresh()
+
+    # initialize subwindow
     titlewin = win.subwin(1, cols ,begin_y + 2, begin_x)
+    titlewin.addstr(0, 0, pretitle) # if a previous post title is supplied load the previous post
+
     bodywin = win.subwin(rows - 7, cols ,begin_y + 4, begin_x)
-    replywin = win.subwin(1, 2, rows - 2, begin_x + 22)
+    bodywin.addstr(0, 0, prebody) # load previous post body
+
+    # refresh previous text
+    bodywin.refresh()
+    titlewin.refresh()
+
+    replywin = win.subwin(1, 5, rows - 2, begin_x + 22)
     redisplay = win.subwin(1, 22, rows - 2, begin_x)
+
     while True:
         title = curses.textpad.Textbox(titlewin, insert_mode=True).edit()
         body = curses.textpad.Textbox(bodywin, insert_mode=True).edit()
-        redisplay.addstr(0, 0, "Exit and post? (y/N) ")
 
+        redisplay.addstr(0, 0, "Exit and save? (y/N) ")
         redisplay.refresh()
-        win.refresh()
-        reply = curses.textpad.Textbox(replywin, insert_mode=True).edit(lambda x: 7 if x == 10 else x)
+
+        reply = curses.textpad.Textbox(replywin, insert_mode=True).edit(lambda x: 7 if x == 10 else x)  # convert Ctrl+G to Enter
+
         if reply.strip() in ['y', 'Y', "yes", "Yes", "YES"]:
             break
-        redisplay.clear()
-        redisplay.refresh()
-        replywin.clear()
-        replywin.refresh()
+        else:  # return to edit post
+            redisplay.clear()
+            redisplay.refresh()
+            replywin.clear()
+            replywin.refresh()
 
     # restore configuration
     curses.nocbreak()
@@ -49,6 +65,11 @@ def editor():
 
     return title, body
 
+def search_display():
+    '''
+    A display for search result
+    '''
+    pass
 
 def search_post(conn, db, uid):
     ''' 
@@ -61,12 +82,51 @@ def post_question(conn, db, uid):
     post a question to the database 
     '''
     title, body = editor()
-    print(title)
+    print("*-----------------------*")
+    print("Please review your post:")
+    print("Title:", title)
+    print("Body:")
     print(body)
+    userInput = input("Do you want to post? (y/N)")
+    if userInput.strip() not in ['y', 'Y', "yes", "Yes", "YES"]:
+        print("User discarded post.")
+        time.sleep(0.5)
+        print("*-----------------------*")
+        return
+    print("Posting...\n")
+    time.sleep(1)
+    maxpid = db.execute("SELECT MAX(pid) FROM posts").fetchall()
+    if maxpid[0][0] != None:
+        maxpidstr = maxpid[0][0][1:]
+        if maxpidstr == "999":
+            print("Maximum posts reached. No more posts can be made.")
+            return
+        pidint = int(maxpidstr) + 1
+        newpid = "p" + str(pidint).zfill(len(maxpidstr))
+    else:
+        newpid = "p000"
+
+    pdate = datetime.date(datetime.now()) # pdate = current date
+
+    db.execute("INSERT INTO posts VALUES (?,?,?,?,?)", (newpid, pdate, title, body, uid)) # insert question into posts table
+    db.execute("INSERT INTO questions VALUES (?, NULL)", (newpid,)) # insert question into questions table. theaid does not exist initially
+    conn.commit()
+
+    check = db.execute("SELECT posts.pid FROM posts WHERE posts.pid = ?", (newpid,))
+    if check != []:
+        print("Question posted successfully!")
+        print()
+        time.sleep(0.5)
+    else:
+        print("Question posting unsuccessful. Please try again later")
+        print()
+        time.sleep(0.5)
+
+    return
 
 def session(conn, db, uid):
     ''' User session entrance '''
-    print("*------------*")
+    print("*-----------------------*")
     print("Welcome back!")
     while True:
         print("Select an action:")
@@ -74,7 +134,7 @@ def session(conn, db, uid):
         print("2. Search for posts.")
         print("3. Logout")
         userInput = input("(1/2/3): ")
-        print("*------------*")
+        print("*-----------------------*")
         userInput.strip()
         if not userInput.isdigit():
             print()
@@ -92,3 +152,14 @@ def session(conn, db, uid):
         elif userInput == 3:
             time.sleep(0.5)
             return
+
+if __name__ == "__main__":
+    title = "Test title"
+    body = ''' 
+    This is a really long body. The purpose of this long body is too test that if the editor is functioning properly. There are a lot of 
+    redunant sentences and words in this string, or you may call it, text. Again, these are just fillers, they dont really mean anything.
+    It is also unnecessarily verbose such that I can test what if the post body is more than a single line
+    '''
+    newtitle, newbody = editor(title, body)
+    print(newtitle)
+    print(newbody)
--- Code/main.py
+++ Code/main.py
@@ -12,10 +12,10 @@ except ImportError as args:
 
 def register(conn, db):
     ''' 
-    Creating new users. Return False if user want to quit the program, True if otherwise.
+    Creating new users.
     '''
     newName = input("Create user name: ").strip()
-    result = db.execute("SELECT users.uid FROM users WHERE users.name = ?", (newName,)).fetchall()
+    result = db.execute("SELECT users.uid FROM users WHERE UPPER(users.name) = UPPER(?)", (newName,)).fetchall()
     if result == []:
         print()
         print("Username Okay")
@@ -23,22 +23,33 @@ def register(conn, db):
         print()
         print("User already exists")
         time.sleep(1)
-        return True
+        return 
 
     # obtain user info
     password = getpass("Enter new password (no blankspace):").strip()
-    city = input("Enter your city: ")
+
+    while True:
+        city = input("Enter your city: ")
+        if city.strip() == '':
+            print("City cannot be empty!")
+        else:
+            break
+
     crdate = datetime.date(datetime.now()) # crdate = current date
 
     # find last uid
     maxuid = db.execute("SELECT MAX(uid) FROM users").fetchall()
-    uidintstr = maxuid[0][0][1:]
-    if uidintstr == "999":
-        print("No more users can be created.")
-        return True
+    if maxuid[0][0] != None:
+        uidintstr = maxuid[0][0][1:]
+        if uidintstr == "999":
+            print("No more users can be created.")
+            return 
 
-    uidint = int(uidintstr) + 1
-    newuid = "u" + str(uidint).zfill(len(uidintstr))
+        uidint = int(uidintstr) + 1
+        newuid = "u" + str(uidint).zfill(len(uidintstr))
+
+    else:
+        newuid = "u000"
 
     db.execute("INSERT INTO users VALUES (?,?,?,?,?)", (newuid, newName, password, city, crdate))
     conn.commit()
@@ -47,30 +58,29 @@ def register(conn, db):
     return SystemFunctions.session(conn, db, newuid)
 
 
-
 def login(conn, db):
     ''' 
-    Existing users login. Return False if user want to quit the program, True if otherwise. 
+    Existing users login.
     '''
-    
+
     userName = input("Enter user name: ").strip() # get username. Remove spaces
     password = getpass("Enter password: ").strip() # get password
-    result = db.execute("SELECT users.uid FROM users WHERE name = ?", (userName,)).fetchall()
+    result = db.execute("SELECT users.uid FROM users WHERE UPPER(name) = UPPER(?)", (userName,)).fetchall()
 
     if result != []:    # non empty return, username is in database
         var = (userName, password)
-        result = db.execute("SELECT users.uid FROM users WHERE name = ? AND pwd = ?", var).fetchall()
+        result = db.execute("SELECT users.uid FROM users WHERE UPPER(name) = UPPER(?) AND pwd = ?", var).fetchall()
 
         if result == []: # indicating the password and username does not match
             print("Username or password is incorrect.")
             time.sleep(1)
-            return True
+            return 
         else:
-            uid = result[0]
+            uid = result[0][0]
     else:
         print("User does not exist.")
         time.sleep(1)
-        return True
+        return 
 
     print("login successful")
 
@@ -98,35 +108,33 @@ def main():
     # connect to database
     conn = sqlite3.connect(db_path)
     db = conn.cursor()
-
-    print("Welcome!")
-    
+    print("*-----------------------*")
     while True:
-        print()
+        print("Welcome to community QAN!")
         print("Please select an action:")
         print("1. Login")
         print("2. Register")
         print("3. Quit")
-        userInput = input("Select your action (1/2/3):")
+        userInput = input("Select your action (1/2/3): ")
         
         userInput.strip()
         if not userInput.isdigit():
             print()
             print("Unrecognized input. Please try again.")
+            print("*-----------------------*")
             time.sleep(1)
             continue
 
         userInput = int(userInput)
         if userInput == 1:
-            if not login(conn, db):
-                exit(0)
-
+            login(conn, db)
+            print("*-----------------------*")
         elif userInput == 2:
-            if not register(conn, db):
-                exit(0)
+            register(conn, db)
+            print("*-----------------------*")
 
         elif userInput == 3:
-            print("Bye")
+            print("GoodBye!")
             exit(0)
         else:
             print()
